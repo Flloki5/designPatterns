4f5f13448763c27ecaaeabda7eb0ea07cc3a2db0
Flloki5 <rozli@op.pl>
2021-04-19 12:33:18 +0200

    Add secend option to implement singleton pattern

--- src/main/java/com/flloki/patterns/Application.java
+++ src/main/java/com/flloki/patterns/Application.java
@@ -1,13 +1,12 @@
 package com.flloki.patterns;
 
 import com.flloki.patterns.creational.builder.ProductDetails;
+import com.flloki.patterns.creational.singleton.Config2;
 import com.flloki.patterns.creational.singleton.Configuration;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-
 
 
 import static com.flloki.patterns.creational.builder.ProductDetails.builder;
@@ -25,6 +24,11 @@ public class Application {
             Configuration config2 = Configuration.getInstance();
 
             System.out.println(config1 == config2);
+
+            Config2 superConfig1 = Config2.SUPER_CONFIG;
+            Config2 superConfig2 = Config2.SUPER_CONFIG;
+
+            System.out.println(superConfig1 == superConfig2); // he already knows ;)
         };
     }
 }
---
+++ src/main/java/com/flloki/patterns/creational/singleton/Config2.java
@@ -0,0 +1,6 @@
+package com.flloki.patterns.creational.singleton;
+
+public enum Config2 {
+
+    SUPER_CONFIG
+}
