a6bd98a2eb656125dec3435317950dc7db599782
Flloki5 <rozli@op.pl>
2021-04-19 12:10:03 +0200

    Implementation of builder pattern

--- src/main/java/com/flloki/patterns/Application.java
+++ src/main/java/com/flloki/patterns/Application.java
@@ -1,9 +1,12 @@
 package com.flloki.patterns;
 
+import com.flloki.patterns.creational.builder.ProductDetails;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import static com.flloki.patterns.creational.builder.ProductDetails.builder;
 
 @SpringBootApplication
 public class Application {
@@ -15,6 +18,15 @@ public class Application {
     public CommandLineRunner runner() {
         return (args) -> {
 
+            ProductDetails productDetails = ProductDetails.builder()
+                    .withName("Chocolate")
+                    .withWeight(100)
+                    .build();
+
+            builder()
+                    .withPrice(50)
+                    .withCategories("Food")
+                    .build();
         };
     }
 }
--- src/main/java/com/flloki/patterns/creational/builder/ProductDetails.java
+++ src/main/java/com/flloki/patterns/creational/builder/ProductDetails.java
@@ -9,4 +9,59 @@ public class ProductDetails {
     private final String producer;
     private final String categories;
 
+    private ProductDetails(Builder builder){
+        this.name = builder.name;
+        this.description = builder.description;
+        this.weight = builder.weight;
+        this.price = builder().price;
+        this.producer = builder().producer;
+        this.categories = builder().categories;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder{
+        private String name = "UNKNOWN";
+        private String description = "UNKNOWN";
+        private int weight = 0;
+        private int price = 0;
+        private String producer = "UNKNOWN";
+        private String categories = "UNKNOWN";
+
+        public Builder withName(String name){
+            this.name = name;
+            return this;
+        }
+
+        public Builder withDescription(String description){
+            this.description = description;
+            return this;
+        }
+
+        public Builder withWeight(int weight){
+            this.weight = weight;
+            return this;
+        }
+
+        public Builder withPrice(int price){
+            this.price = price;
+            return this;
+        }
+
+        public Builder withProducer(String producer){
+            this.producer = producer;
+            return this;
+        }
+
+        public Builder withCategories(String categories){
+            this.categories = categories;
+            return this;
+        }
+
+        public ProductDetails build() {
+            return new ProductDetails(this);
+        }
+    }
 }
