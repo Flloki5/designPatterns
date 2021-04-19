2b61cfe8614e83fb2eb85057d98bcbb819a2bac3
Flloki5 <rozli@op.pl>
2021-04-19 10:42:07 +0200

    Create builder new class

---
+++ src/main/java/com/flloki/patterns/creational/builder/ProductDetails.java
@@ -0,0 +1,12 @@
+package com.flloki.patterns.creational.builder;
+
+public class ProductDetails {
+
+    private final String name;
+    private final String description;
+    private final int weight;
+    private final int price;
+    private final String producer;
+    private final String categories;
+
+}
